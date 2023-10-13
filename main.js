d7c17bd60c3540cd63ee7b6255891c19b5e6ed23
Kristoffer.Kodehode <kristofferb04.kodehode@gmail.com>
2023-10-13 11:36:02 +0200

    add arrows in proj section

--- main.js
+++ main.js
@@ -2,10 +2,16 @@
 
 const homeBtn = document.getElementById("home-btn");
 const projectsBtn = document.getElementById("projects-btn");
+const downBtn = document.getElementById("down");
+const upBtn = document.getElementById("up");
 
 const landingSection = document.getElementById("landing");
 const projectSection = document.getElementById("projects");
+const projectSection2 = document.getElementById("projects2");
+
 const contactSection = document.getElementById("contact");
 
 homeBtn.addEventListener("click", () => navTo(landingSection));
 projectsBtn.addEventListener("click", () => navTo(projectSection));
+downBtn.addEventListener("click", () => navTo(projectSection2));
+upBtn.addEventListener("click", () => navTo(projectSection));
